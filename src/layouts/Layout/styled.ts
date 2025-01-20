e0ca098b008d7eb90295baaaf156375bbcac34d3
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-01-20 19:59:48 +0300

    feat: create layout for pages

---
+++ src/layouts/Layout/styled.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Main = styled.main`
+  padding: 120px 0;
+  flex-grow: 1;
+`;
