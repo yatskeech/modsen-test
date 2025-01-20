49fdda297f2302ce914e9deb3296ac93726cb8b2
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-01-20 13:24:39 +0300

    feat: create a global styles

---
+++ src/styled.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  
+  body {
+    font-family: 'Lexend Deca', 'Inter', sans-serif;
+    color: ${({ theme }) => theme.colors.primary};
+  }
+`;
+
+export default GlobalStyle;
