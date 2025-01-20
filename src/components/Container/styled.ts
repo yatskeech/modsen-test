5b10b91d7abd9a0827739112b86881d93bffbd01
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-01-20 21:45:02 +0300

    refactor: change container structure

--- src/components/Container/styled.ts
+++ src/components/Container/styled.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const StyledContainer = styled.div`
   max-width: 1280px;
   margin: 0 auto;
   padding: 0 16px;
