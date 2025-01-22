06c4e53a0111c875b440ec9bbeb89e97e0e27a77
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-01-22 17:00:50 +0300

    feat: create constants

---
+++ src/constants/index.ts
@@ -0,0 +1,4 @@
+export const ERROR_MESSAGES = {
+  FETCHING: 'Failed to connect to the server. Please check your internet connection and try again later.',
+  NOT_FOUND: 'No artworks found. Please try searching with different keywords or filters.',
+};
