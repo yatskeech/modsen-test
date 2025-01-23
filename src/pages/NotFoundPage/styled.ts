5b5fc0b3749a177ebbaf6b07049bd1a7cc8a4d1d
Dmitry Yatskevich <dima.ja06@gmail.com>
2025-01-23 16:49:25 +0300

    feat: create not found page

---
+++ src/pages/NotFoundPage/styled.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import Status from '@assets/icons/status.svg';
+
+export const StyledText = styled.p`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+  font-size: 20px;
+  text-align: center;
+  margin: 0 auto;
+  padding: 120px 0;
+`;
+
+export const StyledStatus = styled(Status)`
+  width: 80px;
+  height: 80px;
+
+  & path {
+    stroke: ${({ theme }) => theme.colors.error};
+    opacity: 0.8;
+  }
+`;
